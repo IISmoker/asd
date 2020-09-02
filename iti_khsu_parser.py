5aec4cbdf1a9b1e1a60ab8d632e68ad4428f9b9c
hackyouass <79134475418@mail.ru>
2020-09-02 22:36:17 +0700

    added biblioteki

--- iti_khsu_parser.py
+++ iti_khsu_parser.py
@@ -1,2 +1,9 @@
-import lxml.html as html
-from pandas import DataFrame
+from openpyxl import load_workbook
+import os
+
+lxml = './lxml'
+dir_list = [os.path.join(lxml, name_table) for name_table in os.listdir(lxml)]
+if dir_list:
+    date_list = [[x, os.path.getctime(x)] for x in dir_list]
+    sort_date_list = sorted(date_list, key=lambda x: x[1], reverse=True)
+    print(sort_date_list[0][0])
