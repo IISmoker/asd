c7ec155210da55c5a07f5dc5a4161b045ef0c48e
hackyouass <79134475418@mail.ru>
2020-09-03 00:02:28 +0700

    huita

--- probe.py
+++ probe.py
@@ -3,7 +3,7 @@ import xlrd
 table = xlrd.open_workbook('./lxml/vremennoe_raspisanie_iti_03_05.09.2020.xlsx', formatting_info=False)
 sheet = table.sheet_by_index(0)
 group = sheet.row_values(2)[6]
-first_lesson = sheet.row_values(3-4)[6]
+first_lesson = sheet.row_values(4)[6]
 qwe = sheet.row_values(3)[6]
 print(group)
 print(first_lesson)
